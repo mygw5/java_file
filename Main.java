574a966024e3ee0c5af5a6dfd4737747d9a54c4b
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-11-14 23:03:34 +0000

    Add クラス定義

--- Main.java
+++ Main.java
@@ -2,27 +2,30 @@ class Main {
   // クラス部分
   public static void main(String[] args) {
     // メソッド部分
-    int[] numbers = {1, 4, 5, 16, 19};
+  //   int[] numbers = {1, 4, 5, 16, 19};
 
-    int oddSum = 0;
-    int evenSum = 0;
+  //   int oddSum = 0;
+  //   int evenSum = 0;
 
-    for (int number : numbers) {
-      if (number % 2 == 0) {
-        evenSum += number;
-      } else {
-        oddSum += number;
-      }
-    }
+  //   for (int number : numbers) {
+  //     if (number % 2 == 0) {
+  //       evenSum += number;
+  //     } else {
+  //       oddSum += number;
+  //     }
+  //   }
 
-    System.out.println(oddSum);
-    System.out.println(evenSum);
+  //   System.out.println(oddSum);
+  //   System.out.println(evenSum);
 
-    hello ("Bob");
-  }
+  //   hello ("Bob");
+
+    String name = Person.fullName("Elen", "Violet");
+    System.out.println(name);
+   }
 
 
-  public static void hello (String name) {
-    System.out.println(name + "さんこんにちは");
-  }
+  // public static void hello (String name) {
+  //   System.out.println(name + "さんこんにちは");
+  // }
 }
---
+++ Person.java
@@ -0,0 +1,5 @@
+class Person {
+  public static String fullName(String firstName, String lastName) {
+    return firstName + "" + lastName;
+  }
+}
