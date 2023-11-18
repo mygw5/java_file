b8782a49556d7231875da91bbd68c0708d16658f
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-11-18 10:48:57 +0000

    ファイル整理

--- Main.java
+++ Main.java
@@ -1,17 +1,7 @@
-class Main {
-  // クラス部分
-  public static void main(String[] args) {
-    // メソッド部分
-    String name = "Kana";
-    int age = 10;
-    double height = 1.45;
-    double weight = 40;
-    double bmi = weight / height / height;
+ // import java.lang.クラス名　で外部ライブラリを読み込む
 
-    System.out.println("名前は" + name + "です");
-    System.out.println("年齢は" + age + "です");
-    System.out.println("身長は" + height + "mです");
-    System.out.println("体重は" + weight + "kgです");
-    System.out.println("BMIは" + bmi + "です");
-   }
+class Main {
+  public static void main(String[] args) {
+
+  }
 }
--- Person.java
+++
@@ -1,5 +0,0 @@
-class Person {
-  public static String fullName(String firstName, String lastName) {
-    return firstName + "" + lastName;
-  }
-}
