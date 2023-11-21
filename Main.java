d2cfdcb1d6cbeecdcbcf75955184aef3fd789bb5
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-11-21 09:58:14 +0000

    Java練習

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@
 
 class Main {
   public static void main(String[] args) {
-
+    String name = "Test";
+    // print 改行なし
+    System.out.print(name);
+    // line 改行
+    System.out.println(name);
   }
 }
